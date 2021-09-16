f17ec879134b23610a2ff68f28e21b0de405f94b
LmuraroL <73319250+LmuraroL@users.noreply.github.com>
2021-09-16 13:03:46 -0300

    finalizando falta email

--- Portfolio/static/script.js
+++ Portfolio/static/script.js
@@ -9,3 +9,52 @@ menuMobile.addEventListener("click", () => {
         : menuMobile.classList.replace("bi-x", "bi-list-nested");
     body.classList.toggle("menu-nav-active");
 });
+/* FECHA O MENU QUANDO CLICA NA LIST*/
+
+const navItem = document.querySelectorAll('.nav-item')
+navItem.forEach(Item => {
+    Item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active")
+            menuMobile.classList.replace("bi-x", "bi-list-nested");
+        }
+    })
+
+});
+// Animar todos os itens na tela que tiverem meu atributo data-anime
+
+const item = document.querySelectorAll("[data-anime]");
+
+const animeScroll = () => {
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
+};
+
+animeScroll();
+
+window.addEventListener("scroll", () => {
+    animeScroll();
+})
+
+// Ativar carregamento no botão de enviar formulário para
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener("click", () => {
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
+})
+
+// Tira a mensagem de sucesso depois de 5 segundos
+
+setTimeout(() => {
+    document.querySelector('#alerta').style.display = 'none';
+}, 5000)
